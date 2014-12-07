7ae26e7841767b14f0694badbda9587bc33dc3eb
Michael Shull <eshinobicom@gmail.com>
2014-12-06 23:41:48 -0800

    Added Admin index

--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,49 @@
 <?php
 
-// error checking on
+// --------------------------------------------------------
+//             __                             
+//    |       /\ \                            
+//    |       \ \ \      __  __  _ __    __   
+//    !        \ \ \  __/\ \/\ \/\`'__\/'__`\ 
+//    J         \ \ \L\ \ \ \_\ \ \ \ /\  __/ 
+//    _/o        \ \____/\ \____/\ \_\\ \____\
+//  ~~\_)\~~~     \/___/  \/___/  \/_/ \/____/
+// 
+// --------------------------------------------------------
+/*
+
+    Lure Admin : Home Page
+    v.0.1.0
+    
+    By Michael Shull
+    Github: @mshull
+    mshull@g.harvard.edu
+    
+    This code uses the LureClient library to connect to 
+    a local or remote Lure API.
+
+*/
+
+// ----------------------------------
+// Libraries, Sessions and Constants
+// ----------------------------------
+
+// error checking
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // connect to lure
 require 'libs/LureClient.php';
 
-// get users
-$result = getUsers();
+// start sessions
+session_start();
 
-// print results
-echo $result;
+// ----------------------------------
+// Page Logic
+// ----------------------------------
+
+// check for session
+if (!isset($_SESSION["auth"])) {
+	include("templates/signin.html");
+	exit();
+}
