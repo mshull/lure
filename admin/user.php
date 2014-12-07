383fb5b2a4aa5f9464527955bf686d86e69d4f73
Michael Shull <eshinobicom@gmail.com>
2014-12-07 02:01:27 -0800

    Added admin pages

---
+++ admin/user.php
@@ -0,0 +1,53 @@
+<?php
+
+// --------------------------------------------------------
+//             __                             
+//    |       /\ \                            
+//    |       \ \ \      __  __  _ __    __   
+//    !        \ \ \  __/\ \/\ \/\`'__\/'__`\ 
+//    J         \ \ \L\ \ \ \_\ \ \ \ /\  __/ 
+//    _/o        \ \____/\ \____/\ \_\\ \____\
+//  ~~\_)\~~~     \/___/  \/___/  \/_/ \/____/
+// 
+// --------------------------------------------------------
+/*
+
+    Lure Admin : User
+    v.0.1.0
+    
+    By Michael Shull
+    Github: @mshull
+    mshull@g.harvard.edu
+    
+    This code uses the LureClient library to connect to 
+    a local or remote Lure API.
+
+*/
+
+// ----------------------------------
+// Libraries, Sessions and Constants
+// ----------------------------------
+
+// error checking
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// connect to lure
+require 'libs/LureClient.php';
+
+// start sessions
+session_start();
+
+// ----------------------------------
+// Page Logic
+// ----------------------------------
+
+// check for session
+if (!isset($_SESSION["auth"])) {
+	header('Location: index.php');
+	exit();
+}
+
+// show users page
+include("templates/user.html");
+exit();
